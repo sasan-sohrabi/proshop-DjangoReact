e705ad59d4ede6b47e4ece8e44389a7ceb0747e7
sasanpy <sasan.sp92@hotmail.com>
2021-11-02 21:08:19 +0330

    Fetch data from backend to frontend.

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializers
 
 
 @api_view(['GET'])
@@ -12,15 +15,13 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_products(request):
-    return Response(products)
+    product = Product.objects.all()
+    serializers = ProductSerializers(product, many=True)
+    return Response(serializers.data)
 
 
 @api_view(['GET'])
 def get_single_products(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializers = ProductSerializers(product, many=False)
+    return Response(serializers.data)
