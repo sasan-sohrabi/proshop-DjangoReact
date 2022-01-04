8e40a616e1c4ab4df245e211a5d398b1497553cf
sasanpy <sasan.sp92@hotmail.com>
2022-01-04 18:00:12 +0330

    3. User In Navbar & Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,11 +1,41 @@
 import axios from 'axios'
-
 import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
+
     USER_LOGOUT,
-} from "../constants/userConstats";
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
+
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
+
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
+
+    USER_DELETE_REQUEST,
+    USER_DELETE_SUCCESS,
+    USER_DELETE_FAIL,
+
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
+
+} from '../constants/userConstats'
+
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -22,13 +52,13 @@ export const login = (email, password) => async (dispatch) => {
 
         const {data} = await axios.post(
             '/api/users/login/',
-            {'username':email, 'password':password},
+            {'username': email, 'password': password},
             config
         )
 
         dispatch({
-            type:USER_LOGIN_SUCCESS,
-            paload:data
+            type: USER_LOGIN_SUCCESS,
+            paload: data
         })
 
         localStorage.setItem('userInfo', JSON.stringify(data))
@@ -42,4 +72,12 @@ export const login = (email, password) => async (dispatch) => {
         })
 
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
+    dispatch({type: ORDER_LIST_MY_RESET})
+    dispatch({type: USER_LIST_RESET})
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,33 +1,78 @@
-import React from 'react';
-import {Navbar, Nav, Container} from "react-bootstrap";
-import {LinkContainer} from "react-router-bootstrap";
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Navbar, Nav, Container, Row, NavDropdown} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import {logout} from '../actions/userActions'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faShoppingCart, faUser} from '@fortawesome/fontawesome-free-solid'
 
-
 function Header() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand >ProShop</Navbar.Brand>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
                     </LinkContainer>
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="ml-auto">
+
                             <LinkContainer to='/cart'>
-                                <Nav.Link ><FontAwesomeIcon icon={faShoppingCart}/>Cart</Nav.Link>
+                                <Nav.Link><FontAwesomeIcon icon={faShoppingCart}/>Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link ><FontAwesomeIcon icon={faUser}/>Login</Nav.Link>
-                            </LinkContainer>
+
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link><FontAwesomeIcon icon={faUser}/>Login</Nav.Link>
+                                </LinkContainer>
+                            )}
+
+
+                            {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title='Admin' id='adminmenue'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                </NavDropdown>
+                            )}
+
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
-    );
+    )
 }
 
-export default Header;
+export default Header
